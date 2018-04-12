4a179a2e2192ee2d44077c7830fa9dd7aa8afbd4
d761472032 <761472032@qq.com>
2018-04-12 11:40:27 +0800

    springMVC初步框架

---
+++ src/main/java/com/mySpringMVC/controller/primaryController.java
@@ -0,0 +1,9 @@
+package com.mySpringMVC.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/springmvc")
+public class primaryController {
+}
